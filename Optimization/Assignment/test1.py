f9d5452845885ee9c96b0630944d640096e8f98e
tapanauti <tapan.v.auti@gmail.com>
2020-10-27 19:53:45 +0000

    final output

--- Optimization/Assignment/test1.py
+++ Optimization/Assignment/test1.py
@@ -1,28 +1,6 @@
 from ortools.linear_solver import pywraplp, _pywraplp
 
 
-
-
-def const(gnt,solver,var_list,variables):
-    # con_list = solver.Add(var_list[0] + var_list[1] + var_list[2]+ var_list[3] + var_list[4] + var_list[5] + var_list[6] + var_list[7] + var_list[8]+ var_list[9] == dmd[0])
-    # con_list = solver.Add(var_list[0] + var_list[1] + var_list[2]+ var_list[10] + var_list[11] + var_list[12] + var_list[13] + var_list[14] + var_list[15]+ var_list[16] == dmd[1])
-    #for i in range(0,3):
-    for j in range(0,3):
-        con_list = solver.Add(var_list[0] + var_list[1] + var_list[2] + variables[0][j] + variables[1][j] == 400  )
-
-    return con_list
-
-
-def obj(gnt,solver,var_list,con_list):
-
-    objt= solver.Objective()
-
-    for i in range(0,len(gnt)):
-        objt.SetCoefficient(var_list[i],gnt[i][3])
-    objt.SetMinimization()
-       
-    return objt
-
 def solve(solver):
     result = solver.Solve()
     return result
@@ -39,30 +17,58 @@ def prt(solver,objt):
 def main(gnt,dmd):
 
     solver = pywraplp.Solver('Generator',pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-	
-    variables = [['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18','a19','a20','a21','a22','a23','a24'],
-    ['b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b20','b21','b22','b23','b24']]
-    var_list = [solver.NumVar(100,900,'e'), solver.NumVar(100,600,'f'),solver.NumVar(10,100,'g') ]
-    
-    for i in range(0,len(variables)):
-        var_list.extend([solver.NumVar(gnt[i][1],gnt[i][2],str(variables[i][z])) for z in range (0,24)])
+    var_list = []
 
-    print(var_list)
-    con_list = const(gnt,solver,var_list,variables)   
-    objt = obj(gnt,solver,var_list,con_list)
+    A = [solver.NumVar(gnt[0][1],gnt[0][2],f'A{i}') for i in range (1,25)]
+    B = [solver.NumVar(gnt[1][1],gnt[1][2],f'B{i}') for i in range (1,25)]
+    C = [solver.NumVar(gnt[2][1],gnt[2][2],f'C{i}') for i in range (1,25)]
+    D = [solver.NumVar(gnt[3][1],gnt[3][2],f'D{i}') for i in range (1,25)]
+    E = [solver.NumVar(100,900,'E')]
+    F = [solver.NumVar(100,600,'F')]
+    G = [solver.NumVar(10,100,'G')]
+    H = [solver.NumVar(gnt[7][1],gnt[7][2],f'H{i}') for i in range (1,25)]
+    I = [solver.NumVar(0,0,f'I{i}') for i in range (1,7)]
+    I.extend([solver.NumVar(0,35,f'I{i}') for i in range (7,12)])
+    I.extend([solver.NumVar(0,70,f'I{i}') for i in range (12,17)])
+    I.extend([solver.NumVar(0,35,f'I{i}') for i in range (17,20)])
+    I.extend([solver.NumVar(0,0,f'I{i}') for i in range (20,25)])
+    J = [solver.NumVar(0,0,f'J{i}') for i in range (1,7)]
+    J.extend([solver.NumVar(0,10,f'J{i}') for i in range (7,12)])
+    J.extend([solver.NumVar(0,20,f'J{i}') for i in range (12,17)])
+    J.extend([solver.NumVar(0,10,f'J{i}') for i in range (17,20)])
+    J.extend([solver.NumVar(0,0,f'J{i}') for i in range (20,25)])
+
+    var_list.append(A)
+    var_list.append(B)
+    var_list.append(C)
+    var_list.append(D)
+    var_list.extend(E)
+    var_list.extend(F)
+    var_list.extend(G)
+    var_list.append(H)
+    var_list.append(I)
+    var_list.append(J)
+    
+    for j in range (0,24):
+        print(var_list[0][j])
+        solver.Add(var_list[0][j] + var_list[1][j] + var_list[2][j] + var_list[3][j] + var_list[4] + var_list[5] + var_list[6] + var_list[7][j] + var_list[8][j] + var_list[9][j]== dmd[j])
+           
+
+
+
+    objt = solver.Objective()
+    for i in range(0,10):
+        if(i<4 or i>6):
+            for j in range (0,24):
+                objt.SetCoefficient(var_list[i][j],gnt[i][3])
+    objt.SetCoefficient(var_list[4],gnt[4][3])
+    objt.SetCoefficient(var_list[5],gnt[5][3])
+    objt.SetCoefficient(var_list[6],gnt[6][3])
+    objt.SetMinimization()   
+    
+    
     result = solve(solver)
     prt(solver,objt)
-	
-	
-   
-     
-   
-
-    return{'var_list':var_list,
-            'con_list':con_list,
-            'objt':objt,
-            'solver':solver,
-            'result':result}
 
 if __name__ == '__main__':
 
